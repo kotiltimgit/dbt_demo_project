6a8ed395ef6df2f11594c5b78b5217dc0e4f3cb1
Koteswar Rao K <koteswarrao.k@lntinfotech.com>
2024-10-15 11:47:38 +0000

    commit run custom query changes

--- macros/custom_materialization/run_custom_query.sql
+++ macros/custom_materialization/run_custom_query.sql
@@ -3,6 +3,7 @@
   {% call statement("custom_run_query_statement", fetch_result=true, auto_begin=false) %}
     {{ sql }}
   {% endcall %}
- 
+
   {% do return(load_result("custom_run_query_statement")) %}
+  
 {% endmacro %}
