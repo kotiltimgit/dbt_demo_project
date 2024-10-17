1b42e918e177b890b372d09bfb9427cb1d6b68ba
Syed-Ahamed-S <146360656+Syed-Ahamed-S@users.noreply.github.com>
2024-10-17 17:35:31 +0530

    Revert "Development"

--- macros/custom_materialization/run_custom_query.sql
+++ macros/custom_materialization/run_custom_query.sql
@@ -3,7 +3,11 @@
   {% call statement("custom_run_query_statement", fetch_result=true, auto_begin=auto_begin) %}
     {{ sql }}
   {% endcall %}
-
+ 
   {% do return(load_result("custom_run_query_statement")) %}
+{% endmacro %}
 
+{% macro job_schedules_macro() %}
+  {% set Table_Name = 'JOB_SCHEDULES' %}
+  {{ Table_Name }}
 {% endmacro %}
--- models/gold/data_observability/vw_job_schedules.sql
+++ models/gold/data_observability/vw_job_schedules.sql
@@ -1,13 +1,12 @@
--- depends_on: {{ ref('dbt_job_schedules') }}
+-- depends_on: {{ ref('dbt_schedules_test') }}
 
 {{
     config(
-        materialized='view', 
+        materialized='view',
         database='DBT_DB_DEV',
         schema='GOLD',
         alias='VW_JOB_SCHEDULES'
     )
 }}
 
-select 
-* from {{ ref('job_schedules') }}
+select * from {{ ref('job_schedules') }}
